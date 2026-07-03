b1035a4d83b0e027f9aaf76b128d5c68603e1537
唐超 <419704299@qq.com>
2026-07-03 15:23:43 +0800

    feat: 第1-2篇AI文章 - 大模型是什么 & 从One-Hot到词嵌入

--- docs/.vitepress/menuAISidebar.js
+++ docs/.vitepress/menuAISidebar.js
@@ -7,6 +7,8 @@ export default [
     collapsed: false,
     items: [
       { text: "理解 Token", link: `${commonPath}/token.md` },
+      { text: "大模型是什么", link: `${commonPath}/what-is-llm.md` },
+      { text: "从One-Hot到词嵌入", link: `${commonPath}/one-hot-to-embedding.md` },
     ],
   },
 ];
